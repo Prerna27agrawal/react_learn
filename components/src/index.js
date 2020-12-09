cb960a9bc7dc77b1deb3e774286b54bac845e911
Prerna27agrawal <agrawalprerna1999@gmail.com>
2020-12-09 21:44:01 +0530

    nested components

--- components/src/index.js
+++ components/src/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
     <div className="ui container comments">
-        <CommentDetail author="Sam"/>
-        <CommentDetail author="Alex"/>
-        <CommentDetail author="Jane"/>
+        <ApprovalCard>
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="today at 4:45" 
+                comment="comment1"
+                image={faker.image.image()}
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                author="Alex" 
+                timeAgo="today at 2:00 am"
+                comment="comment2" 
+                image={faker.image.image()}
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail
+                author="Jane" 
+                timeAgo="yesterday 5:00pm" 
+                comment="comment3"
+                image={faker.image.image()}
+            />
+        </ApprovalCard>
     </div>
     )
 };
