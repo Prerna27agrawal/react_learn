dfb9099f3173ec21373a64500f0e0afb8450dffe
Prerna27agrawal <agrawalprerna1999@gmail.com>
2020-12-14 22:34:26 +0530

    search  and grid sys

--- pics/src/Component/ImageList.js
+++ pics/src/Component/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = props =>{
    const images =  props.images.map((image)=>{
-     return <img  key={image.id} src={image.urls.regular} alt="not found"/>
+     return <ImageCard  key={image.id}
+          image={image}/> 
    });
     // console.log(props.images);
     return <div className="image-list">{images}</div>
